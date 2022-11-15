9a34a518741e5a6c844d7c2a6970bd60f97dea38
Szymon Wajda <wajda2001@gmail.com>
2022-11-15 18:52:10 +0100

    Implement item_model

---
+++ lib/models/item_model.dart
@@ -0,0 +1,12 @@
+class ItemModel {
+  ItemModel({
+    required this.id,
+    required this.title,
+    required this.urlImg,
+    required this.releaseDate,
+  });
+  final String id;
+  final String title;
+  final String urlImg;
+  final DateTime releaseDate;
+}

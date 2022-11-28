5329a9768c7dd294d5ec334fdc06fbb5a89c4426
Szymon Wajda <wajda2001@gmail.com>
2022-11-28 17:06:13 +0100

    Improve displaying dates in the app

--- lib/models/item_model.dart
+++ lib/models/item_model.dart
@@ -1,3 +1,5 @@
+import 'package:intl/intl.dart';
+
 class ItemModel {
   ItemModel({
     required this.id,
@@ -12,5 +14,9 @@ class ItemModel {
 
   String dayLeft() {
     return releaseDate.difference(DateTime.now()).inDays.toString();
+  }
+
+  String releaseDateFormated() {
+    return DateFormat.yMEd().format(releaseDate);
   }
 }
